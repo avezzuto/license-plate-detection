8900e27ff2a6903ccbd6ee659122c1d8a950707c
Andrea Vezzuto <a.vezzuto@student.tudelft.nl>
2024-01-05 16:39:56 +0100

    Completed most of recognise pipeline, parameter optimisations need to be made as well as proper scene change detection

--- Rotation.py
+++ Rotation.py
@@ -4,9 +4,10 @@ from scipy import ndimage
 
 
 def rotate(images):
-    showImages = False
-
     rotated_plates = []
+    threshold = 100
+    if len(images) > 1:
+        threshold = 30
 
     for idx, image in enumerate(images):
         # Ensure the image has non-zero dimensions
@@ -15,33 +16,30 @@ def rotate(images):
             gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
             # Perform edge detection using Canny
-            edges = cv2.Canny(gray_image, 50, 150)
-
-            if showImages:
-                # Display edges for visualization
-                cv2.imshow(f'Edges of plate {idx}', edges)
+            edges = cv2.Canny(gray_image, 100, 150)
 
             # Detect lines using Hough Transform
-            lines = cv2.HoughLines(edges, 1, np.pi / 180, 100)
+            lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold)
 
-            # Calculate the rotation angle from detected lines
-            angle = 0
+            if lines is not None:
+                angles = [np.degrees(line[0][1]) for line in lines]
 
-            if lines is not None and len(lines) > 0:
-                angle_rad = lines[0][0][1]  # Angle in radians
-                angle = np.degrees(angle_rad)  # Convert radians to degrees
+                # Calculate the most common angle (mode)
+                if angles:
+                    angle = max(set(angles), key=angles.count)
 
-                # Adjust angle to match landscape orientation
-                angle -= 90
-                # Ensure angle is within -90 to 90 degrees range
-                if angle >= 90:
+                    # Adjust angle to match landscape orientation
                     angle -= 90
-                elif angle <= -90:
-                    angle += 90
+                    # Ensure angle is within -90 to 90 degrees range
+                    if angle >= 90:
+                        angle -= 90
+                    elif angle <= -90:
+                        angle += 90
 
-            # Rotate the image based on the calculated angle
-            rotated_img = ndimage.rotate(image, angle, reshape=False)
-            rotated_plates.append(rotated_img)
+                    # Rotate the image based on the calculated angle
+                    image = ndimage.rotate(image, angle, reshape=False)
+
+            rotated_plates.append(image)
 
     return rotated_plates
 
