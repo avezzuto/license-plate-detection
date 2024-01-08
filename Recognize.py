46ca53476286c4cad9088d7d51f23f3a14c8c0ac
Matej Kliment <M.Kliment@student.tudelft.nl>
2024-01-08 12:29:49 +0100

    added character grouping numbers/letters

--- Recognize.py
+++ Recognize.py
@@ -80,13 +80,13 @@ def segment_and_recognize(plate_image):
 	eroded = cv2.erode(binarised, structuring_element)
 	dilated = cv2.dilate(eroded, structuring_element)
 	dilatedClosing = cv2.dilate(dilated, structuring_element)
-	eroded = cv2.erode(dilatedClosing, structuring_element)
+	img = cv2.erode(dilatedClosing, structuring_element)
 
 	indices_to_start = []
 	indices_to_end = []
 	started = False
-	for i in range(eroded.shape[1]):
-		section = eroded[:, i]
+	for i in range(img.shape[1]):
+		section = img[:, i]
 		unique_colors = np.unique(section)
 		if 255 in unique_colors:
 			if not started:
@@ -99,20 +99,20 @@ def segment_and_recognize(plate_image):
 
 	# Add the last section if it ends with white pixels
 	if started:
-		indices_to_end.append(eroded.shape[1])
+		indices_to_end.append(img.shape[1])
 
 	chars = []
 	hyphen_pos = []
 	count = 0
 	for start, end in zip(indices_to_start, indices_to_end):
-		segment_mask = eroded[:, start:end]
+		segment_mask = img[:, start:end]
 		if np.count_nonzero(segment_mask) > 100:
 			chars.append(segment_mask)
 			count += 1
 		elif np.count_nonzero(segment_mask) > 50:
 			hyphen_pos.append(count)
 	if showImages:
-		cv2.imshow("Binarised mask", eroded)
+		cv2.imshow("Binarised mask", img)
 
 	letters = read('dataset/SameSizeLetters')
 	numbers = read('dataset/SameSizeNumbers')
@@ -120,9 +120,24 @@ def segment_and_recognize(plate_image):
 	mapping = create_dutch_license_plate_mapping()
 	plate = ""
 
+	UseCharGrouping = True
+
+	# Each of 3 sections of a dutch licence plate consist on only letters or only numbers
+	groups = []
+	pre_pos = 0
+	for pos in hyphen_pos:
+		if(pos == 0):
+			continue
+		group = [pre_pos, pos]
+		groups.append(group)
+		pre_pos = pos
+	groups.append([pre_pos, len(chars)])
+
+	letterFit = []
+	numberFit = []
+
 	for i, char in enumerate(chars):
-		minChar = ""
-		minDiff = 1000000
+		
 
 		# Get rid of black pixels around the character
 		positions = np.nonzero(char)
@@ -155,7 +170,9 @@ def segment_and_recognize(plate_image):
 
 		if showImages:
 			cv2.imshow(f'Char {i}', char)
-		print(" Doing char " + str(i))
+
+		minChar = ""
+		minDiff = 1000000
 		for idx, number in enumerate(numbers):
 			number = number[:, :, 0]
 			if char.shape == number.shape:
@@ -163,27 +180,49 @@ def segment_and_recognize(plate_image):
 				diff = np.count_nonzero(xor)
 				#cv2.imshow(f'Difference with number {idx}', xor)
 				if diff < minDiff:
-					print("for numbers the closest is " + str(idx))
 					minDiff = diff
 					minChar = idx
+		numberFit.append((minChar, minDiff))
 
-		for index, letter in enumerate(letters):
+		minChar = ""
+		minDiff = 1000000
+		for idx, letter in enumerate(letters):
 			letter = letter[:, :, 0]
 			if char.shape == letter.shape:
 				xor = cv2.bitwise_xor(char, letter)
 				diff = np.count_nonzero(xor)
 				#cv2.imshow(f'Difference with letter {mapping[index]}', xor)
 				if diff < minDiff:
-					print("for letters the closest is "+ str(mapping[index]))
 					minDiff = diff
-					minChar = mapping[index]
+					minChar = mapping[idx]
+		letterFit.append((minChar, minDiff))
 
-		plate += str(minChar)
-		print("Final min char is " + str(minChar))
+	# Decisions made based on the group od letters/numbers
+	for idx, group in enumerate(groups):
+		sumNumDif = 0
+		sumLetDif = 0
+		for i in range(group[0], group[1]):
+			sumNumDif += numberFit[i][1]
+			sumLetDif += letterFit[i][1]
+		if(sumNumDif < sumLetDif):
+			for i in range(group[0], group[1]):
+				plate += str(numberFit[i][0])
+		else:
+			for i in range(group[0], group[1]):
+				plate += str(letterFit[i][0])
+		if(idx < 2):
+			plate += "-"
+
+	if(not UseCharGrouping):
+		plate = ""
+		for idx in range(len(chars)):
+			if(idx in hyphen_pos and idx != 0 and idx != 6):
+				plate += "-"
+			if(letterFit[idx][1] < numberFit[idx][1]):
+				plate += str(letterFit[idx][0])
+			else:
+				plate += str(numberFit[idx][0])
+
+
 	print("product: " + plate)
-
-	hyphen_pos.sort(reverse=True)
-	for pos in hyphen_pos:
-		plate = plate[:pos] + '-' + plate[pos:]
-
 	return plate
